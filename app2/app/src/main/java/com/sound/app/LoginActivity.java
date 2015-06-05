17d20bd04c3fd2badf606966a4558a15554bb087
Francis Kim <kims_0121@naver.com>
2015-06-05 15:08:07 +0900

    Google OAuth 서버연동 끝

--- app2/app/src/main/java/com/sound/app/LoginActivity.java
+++ app2/app/src/main/java/com/sound/app/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.sound.app;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -10,8 +12,10 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cocosw.bottomsheet.BottomSheet;
 import com.sound.app.auth.Auth;
 import com.sound.app.bonacell.BonacellAsyncTask;
+import com.sound.app.util.BackPressCloseHandler;
 import com.sound.app.weather.WeatherHttpClient;
 
 
@@ -19,16 +23,45 @@ public class LoginActivity extends ActionBarActivity {
 
     private static final String TAG = "LoginActivity";
 
-    TextView oauth;
-    TextView weather;
+    private TextView oauth;
+    private TextView weather;
+    private String id;
+
+    private BottomSheet bottomSheet;
+    private BackPressCloseHandler backPressCloseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        this.bottomSheet = new BottomSheet.Builder(this, R.style.BottomSheet_StyleDialog).title("Option").sheet(R.menu.list).listener(new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+
+                    case R.id.logout:
+                        SharedPreferences pref = LoginActivity.this.getSharedPreferences("userInfo", 0);
+                        SharedPreferences.Editor prefEdit = pref.edit();
+                        prefEdit.putString("id","");
+                        prefEdit.commit();
+
+                        Intent idCheck = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(idCheck);
+                        LoginActivity.this.finish();
+                        break;
+
+                }
+            }
+        }).build();
+
+        this.backPressCloseHandler = new BackPressCloseHandler(this);
+
         Intent intent = getIntent();
         Auth auth = intent.getParcelableExtra("auth");
+
+        SharedPreferences pref = getSharedPreferences("userInfo", 0);
+        this.id = pref.getString("id", "");
 
         oauth = (TextView) findViewById(R.id.oauth);
         oauth.setText(auth.toString());
@@ -36,7 +69,15 @@ public class LoginActivity extends ActionBarActivity {
         weather = (TextView) findViewById(R.id.weather);
 
         new WeatherAsyncTask().execute();
+
+
     }
+
+    @Override
+    public void onBackPressed() {
+        this.backPressCloseHandler.onBackPressed(this.bottomSheet);
+    }
+
 
     private class WeatherAsyncTask extends AsyncTask<Void, Void, String>{
         @Override
