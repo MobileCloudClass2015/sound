3d60927478fb920850ad7da220312af3743080d1
Francis Kim <kims_0121@naver.com>
2015-06-07 18:48:49 +0900

    Make Auth management table

--- server/src/main/java/com/sound/server/admin/AdminController.java
+++ server/src/main/java/com/sound/server/admin/AdminController.java
@@ -1,12 +1,19 @@
 package com.sound.server.admin;
 
+import com.sound.server.auth.Auth;
+import com.sound.server.auth.AuthSearchFilter;
+import com.sound.server.auth.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by Francis on 2015-06-07.
@@ -18,13 +25,18 @@ public class AdminController {
     
     private static Logger logger = LoggerFactory.getLogger(AdminController.class);
     
+    @Autowired
+    private AuthService authService;
+    
     @RequestMapping(value = "/api", method = RequestMethod.GET)
     public String apiPage(Model model){
         return "manage/api";
     }
     
     @RequestMapping(value = "/auth", method = RequestMethod.GET)
-    public String authManagePage(Model model){
+    public String authManagePage(Model model, @ModelAttribute AuthSearchFilter authSearchFilter){
+        List<Auth> authList = authService.selectAuths(authSearchFilter);
+        model.addAttribute("authList", authList);
         return "manage/auth";
     }
 }
--- server/src/main/java/com/sound/server/auth/AuthServiceImpl.java
+++ server/src/main/java/com/sound/server/auth/AuthServiceImpl.java
@@ -1,8 +1,10 @@
 package com.sound.server.auth;
 
+import com.sound.server.util.Pagination;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,4 +41,20 @@ public class AuthServiceImpl extends SqlSessionDaoSupport implements AuthService
     public Auth selectAuth(String id) {
         return getSqlSession().selectOne("auth.selectAuth", id);
     }
+
+    @Override
+    public List<Auth> selectAuths(AuthSearchFilter authSearchFilter) {
+        Pagination pagination = authSearchFilter.getPagination();
+        int count = selectCountAuths(authSearchFilter);
+        pagination.setNumItems(count);
+        if(count == 0){
+            return new ArrayList<Auth>();
+        }
+        return getSqlSession().selectList("auth.selectAuths", authSearchFilter);
+    }
+
+    private int selectCountAuths(AuthSearchFilter authSearchFilter) {
+        return getSqlSession().selectOne("auth.selectCountAuths", authSearchFilter);
+    }
+
 }
