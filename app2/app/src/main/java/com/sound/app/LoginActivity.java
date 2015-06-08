ffa8b55c79a766dfe3747cbd543fda1db333447d
dusgnlfl <dusgnlfl@naver.com>
2015-06-08 18:54:16 +0900

    들은 노래 리스트 작업

--- app2/app/src/main/java/com/sound/app/LoginActivity.java
+++ app2/app/src/main/java/com/sound/app/LoginActivity.java
@@ -8,9 +8,13 @@ import android.content.SharedPreferences;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cocosw.bottomsheet.BottomSheet;
@@ -25,6 +29,8 @@ import com.sound.app.weather.WeatherAsyncTask;
 import com.sound.app.weather.WeatherHttpClient;
 import com.sound.app.youtube.VideoListDemoActivity;
 
+import java.util.ArrayList;
+
 public class LoginActivity extends Activity {
 
     private static final String TAG = "LoginActivity";
@@ -36,6 +42,7 @@ public class LoginActivity extends Activity {
     private ImageView imgview;
     private TextView weatherinfo;
 
+    ArrayList<Content> manylist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,7 +83,20 @@ public class LoginActivity extends Activity {
             this.gpsLocationInfo.showSettingsAlert();
         }
         imgview=(ImageView)findViewById(R.id.weatherimg);
+        weatherinfo=(TextView)findViewById(R.id.weatherinfo);
         new WeatherAsyncTask(getApplicationContext(), this.gpsLocationInfo, this.imgview, this.weatherinfo).execute();
+
+        manylist=new ArrayList<Content>();
+        Content con;
+        con=new Content(R.drawable.ic_launcher, "김연상"); manylist.add(con);
+        con=new Content(R.drawable.ic_launcher, "롤로"); manylist.add(con);
+        con=new Content(R.drawable.ic_launcher, "루리"); manylist.add(con);
+
+        ConlistAdapter ConAdapter=new ConlistAdapter(this,R.layout.context,manylist);
+
+        ListView conlist;
+        conlist=(ListView)findViewById(R.id.list);
+        conlist.setAdapter(ConAdapter);
     }
 
     @Override
@@ -88,5 +108,53 @@ public class LoginActivity extends Activity {
     @Override
     public void onBackPressed() {
         this.backPressCloseHandler.onBackPressed(this.bottomSheet);
+    }
+}
+class Content {
+    Content(int aicon, String atext) {
+        icon=aicon;
+        text=atext;
+    }
+    int icon;
+    String text;
+}
+
+class ConlistAdapter extends BaseAdapter {
+    Context lefticon;
+    LayoutInflater Inflater;
+    ArrayList<Content> asrc;
+    int layout;
+
+    public ConlistAdapter(Context context, int alayout, ArrayList<Content> bsrc) {
+        lefticon=context;
+        Inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        asrc=bsrc;
+        layout=alayout;
+    }
+
+    public int getCount() {
+        return asrc.size();
+    }
+
+    public String getItem(int position) {
+        return asrc.get(position).text;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos=position;
+        if(convertView==null) {
+            convertView=Inflater.inflate(layout, parent, false);
+        }
+        ImageView img=(ImageView)convertView.findViewById(R.id.img);
+        img.setImageResource(asrc.get(position).icon);
+
+        TextView txt=(TextView)convertView.findViewById(R.id.text);
+        txt.setText(asrc.get(position).text);
+
+        return convertView;
     }
 }
